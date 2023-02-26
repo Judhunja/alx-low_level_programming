75ccbe0ef7d06d2248d2ae37bd480ffa6de0af20
Judhunja <judxhunja@gmail.com>
2023-02-26 06:22:15 -0800

    printing a statement using printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-/** 
+/**
  * main - This prints with proper grammar, but the outcome is a piece of art,
- * return: This returns 0
+ * Return: always 0 (success)
  */
 
-int main(void)  
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
