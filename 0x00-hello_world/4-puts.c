264c666b91c1888a26d486db654a52037515297e
Judhunja <judxhunja@gmail.com>
2023-02-26 06:19:29 -0800

    printing a statement with puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,13 @@
-#!/bin/bash
-puts(programming is like building a multilingual puzzle)
+#include <stdio.h>
+
+/**
+* main - prints out a statement
+* puts - outputs the statement
+* Return: always 0 (success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
