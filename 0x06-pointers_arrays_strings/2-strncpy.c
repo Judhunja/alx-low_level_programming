6635aba16f10c70b49213c9b50b08decc036205d
Judhunja <judxhunja@gmail.com>
2023-03-02 14:45:23 -0800

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * _strncpy - copies a string to another string
+  * @dest: one string
+  *
+  * @src: another string
+  *
+  * @n: number of characters to be copied
+  * Return: void
+  *
+  *
+  */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; *dest != '\0' && i < n; i++)
+	{
+		src[i] = dest[i];
+	}
+	src[i] = '\0';
+}
