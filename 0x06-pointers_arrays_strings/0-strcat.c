b513c0daf9363a8587617db40b28c2b2c13f2775
Judhunja <judxhunja@gmail.com>
2023-03-02 12:15:05 -0800

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *_strcat -concatenates two strings
+ *dest: string
+ *src: string
+ *
+ *
+ *
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i;
+
+	for(i = 0; i <= 4; i++)
+	{
+		dest[5 + i] = src[i];
+	}
+	return (dest);
+}
