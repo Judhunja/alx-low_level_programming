a5392be97c166b322eec524e5367c1b767093565
Judhunja <judxhunja@gmail.com>
2023-03-02 13:58:35 -0800

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -5,11 +5,18 @@
   * @dest: maybe a pointer to a string, i dont know
   * @src: mmsdnib
   *
+  * @n: number of bytes from src to be appended onto dest
+  *
   * Return: a pointer to the resulting string dest
   */
 
 char *_strncat(char *dest, char *src, int n)
 {
+	int i;
 
-
-
+	for (i = 0; i < n; i++)
+	{
+		dest[5 + i] = src[i];
+	}
+	return (dest);
+}
