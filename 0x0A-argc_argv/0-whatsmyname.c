50ac057c14965880bb45c7e8dc92c0ee64de5869
Judhunja <judxhunja@gmail.com>
2023-03-13 13:35:26 -0700

    printing the function name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+  * program prints its name, followed by a new line
+  *
+  *
+  * Return: always zero (success)
+  *
+  */
+
+int main (int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; argc < i; i++)
+	{
+		printf ("%s\n",argv[i]);
+		return (0);
+	}
+}
