277670b78a16614d008ad001e9db08be5ec97ccb
Judhunja <judxhunja@gmail.com>
2023-03-21 13:52:55 -0700

    defining a structure with three elements

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,19 @@
 #ifndef DOG_H
 #define DOG_H
 
-#define struct dog { \
-	char name; \
-	float age; \
-	char owner; \
+/**
+ * struct dog - defines name, age and owner of dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
 };
 
 #endif
