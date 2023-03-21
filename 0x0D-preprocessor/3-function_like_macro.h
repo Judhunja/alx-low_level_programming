d44f55b327d9fe45e9f99f67f6f70b8662741435
Judhunja <judxhunja@gmail.com>
2023-03-21 10:51:57 -0700

    printing the absolute value of a number

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,11 +1,6 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
 
-#define ABS(x) do { \
-	if (x < 0) \
-	printf("%d", x + -(2 * (x))); \
-	else \
-	printf("%d", x); \
-} while (0)
+#define ABS(x) ((x) < 0 ? -(x) : (x))
 
 #endif
