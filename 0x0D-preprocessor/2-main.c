8a9f33fde1d5910b9b583f8f2e34ac8471dc0d97
Judhunja <judxhunja@gmail.com>
2023-03-21 10:03:42 -0700

    printing the name of the file I was compiled from

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,3 +1,13 @@
 #include <stdlib.h>
 
-__FILE__
+/**
+ * main - prints the file from which the current file was compiled
+ * Return: always 0(Success)
+ *
+ */
+
+main()
+{
+	printf("%s\n", __FILE__)
+	return (0);
+}
