fdad1c644c9cdaeaacd5c12a814a3224f43310df
Judhunja <judxhunja@gmail.com>
2023-03-23 16:59:02 -0700

    printing numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,6 +1,9 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * print_numbers - prints numbers, followed by anew line
  * @separator: string to be printed between numbers
@@ -8,44 +11,26 @@
  * va_start - initializes and declares arguments in va_list
  * va_arg - retrieves next argument from va_list
  * va_end - cleans up va_list
- * va_list - declares an object that can hold a variable number of arguments
+ * va_list - declares an object that can hold a variable number of
+ * arguments
  */
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
-	int num;
 	va_list args;
 
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
 	{
-		num = va_arg(args, int);
-		if (i > 0 && separator != NULL && separator[0] != '\0')
-		{
-			while (*separator)
-				_putchar(*separator++);
-		}
-
-	if (num < 0)
+		printf("%d", va_arg(args, int));
+	}
+	if (separator != NULL && i < n - 1)
 	{
-		_putchar('-');
-		num = -num;
+		printf("%s",separator);
 	}
-	else if (num == 0)
-	{
-		_putchar('0');
-		return;
-	}
-	else
-	{
-		(num / 10);
-		print_numbers(separator, num / 10);
-	}
-	_putchar(num % 10 + '0');
-	}
-	_putchar('\n');
 	va_end(args);
+	printf("\n");
 }
 
