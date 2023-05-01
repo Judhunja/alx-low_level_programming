e7429c064ed7bfc392b27d542bc86acfd1f75dd1
judhunja <judxhunja@gmail.com>
2023-05-01 06:47:55 -0700

    deleting a node at the nth position of a linked lid list

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+/**
+ * delete_nodeint_at_index - deletes node at index 'index' of a
+ * singly linked list
+ * @head: pointer to pointer to the first node of the linked list
+ * @index: index of the node that should be deleted
+ * Return: 1 if success, 2 if failed
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *current = *head;
+	listint_t *previous = *head;
+	unsigned int pos;
+
+	if (*head == NULL)
+	{
+		return (-1);
+	}
+
+	if (index == 0)
+	{
+		*head = current->next;
+		free(current);
+		return (1);
+	}
+	for (pos = 0; pos < index - 1; pos++)
+	{
+		if (previous == NULL || previous-> next == NULL)
+			return(-1);
+		previous = previous->next;
+	}
+	current = previous->next;
+	if (current == NULL)
+		return (-1);
+
+	previous->next = current->next;
+	free(current);
+	return (1);
+}
