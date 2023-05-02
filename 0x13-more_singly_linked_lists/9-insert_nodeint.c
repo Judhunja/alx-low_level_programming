8e974637f7bb8d213ef648526361900c821aec5a
judhunja <judxhunja@gmail.com>
2023-05-02 08:10:31 -0700

    adding a node at a specified position on a singly linked list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -40,7 +40,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (temp == NULL)
 	{
 		free(newnode);
-		return(NULL);
+		return (NULL);
 	}
 	newnode->next = temp->next;
 	temp->next = newnode;
