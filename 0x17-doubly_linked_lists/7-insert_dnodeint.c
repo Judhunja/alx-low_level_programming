3ab859b1ea9f4c276705df42051d3bf85ff14073
Judhunja <judxhunja@gmail.com>
2023-06-16 00:10:29 +0300

    inserting a new node at a given position in a doubly linked list

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -20,6 +20,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	newnode->n = n;
 	newnode->next = NULL;
 
+	if (*h == NULL && idx != 0)
+		return (NULL);
 	if (idx == 0)
 	{
 		newnode->next = *h;
@@ -33,7 +35,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		ptr = ptr->next;
 		i++;
 	}
-	if (ptr == NULL && i < idx)
+	if (ptr == NULL)
 	{
 		free(newnode);
 		return (NULL);
