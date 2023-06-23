0bc3bc6c9f91c9ea416366d52881f43df11eadec
Judhunja <judxhunja@gmail.com>
2023-06-24 00:35:02 +0300

    loading make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -qO libatoi.so https://github.com/Judhunja/alx-low_level_programming/raw/main/0x18-dynamic_libraries/libatoi.so
-LD_PRELOAD=./libatoi.so
+wget -qO /tmp/libatoi.so https://github.com/Judhunja/alx-low_level_programming/raw/main/0x18-dynamic_libraries/libatoi.so
+LD_PRELOAD=/tmp/libatoi.so
