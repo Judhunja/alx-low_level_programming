095aeafaa81dd9b22590e009ef8f6155806bbca6
Judhunja <judxhunja@gmail.com>
2023-07-07 21:00:23 +0300

    adds an element to a hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,5 +1,4 @@
 #include "hash_tables.h"
-
 /**
  * hash_table_set - adds an element to the hash table
  * @ht: pointer to hashtable
@@ -7,18 +6,15 @@
  * @value: value associated with key(must be duplicated)
  * Return: 1 if success, 0 if failure
  */
-
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new_elem;
 	hash_node_t *current;
 	unsigned int index;
-	char *new_val;
 
 	if (key == NULL || ht == NULL || value == NULL
 			|| key[0] == '\0' || strcmp(key, "") == 0)
 		return (0);
-
 	new_elem = malloc(sizeof(hash_node_t));
 	index = key_index((unsigned char *)key, ht->size);
 	if (new_elem == NULL)
@@ -32,17 +28,19 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	}
 	if (ht->array[index] == NULL)
+	{
 		ht->array[index] = new_elem;
+		return (1);
+	}
 	current = ht->array[index];
 	while (current != NULL)
 	{
 		if (strcmp(current->key, key) == 0)
 		{
 			free(current->value);
-			new_val = strdup(value);
-			if (new_val == NULL)
+			current->value = strdup(value);
+			if (current->value == NULL)
 				return (0);
-			current->value = new_val;
 			return (1);
 		}
 		current = current->next;
