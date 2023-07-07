c13d4f9f6539c4fd919dc7eb1a44c2d3f099b7e0
Judhunja <judxhunja@gmail.com>
2023-07-07 19:15:00 +0300

    adds an element to the hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,18 +10,20 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new_elem, *current;
+	hash_node_t *new_elem;
+	hash_node_t *current;
 	unsigned int index;
+	char *new_val;
 
-	if (key == NULL || ht == NULL)
+	if (key == NULL || ht == NULL || key[0] == '\0')
 		return (0);
 
 	new_elem = malloc(sizeof(hash_node_t));
 	index = key_index((unsigned char *)key, ht->size);
-	current = ht->array[index];
 
 	new_elem->key = strdup(key);
 	new_elem->value = strdup(value);
+	new_elem->next = NULL;
 
 	if (new_elem->key == NULL)
 	{
@@ -30,29 +32,21 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(new_elem);
 		return (0);
 	}
-
-	if (current == NULL)
+	current = ht->array[index];
+	while (current != NULL)
 	{
-		new_elem->next = NULL;
-		ht->array[index] = new_elem;
-	}
-	else
-	{
-		while (current != NULL)
-		{	
-			if (strcmp(current->key, key) == 0)
-			{
-				free(new_elem->key);
-				free(new_elem->value);
-				free(new_elem);
-				strcpy(ht->array[index]->value, value);
-			}
-			else
-			{
-				new_elem->next = ht->array[index]->next;
-				ht->array[index] = new_elem;
-			}
+		if (strcmp(current->key, key) == 0)
+		{
+			free(current->value);
+			new_val = strdup(value);
+			if (new_val == NULL)
+				return (0);
+			current->value = new_val;
+			return (1);
 		}
+		current = current->next;
 	}
+	new_elem->next = ht->array[index];
+	ht->array[index] = new_elem;
 	return (1);
 }
