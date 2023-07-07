98a034f71ade4e23d9fadbb1f33ff9ee905deb14
Judhunja <judxhunja@gmail.com>
2023-07-07 04:56:11 +0300

    adds an element to the hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -36,7 +36,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(ht->array[index]->key);
 		free(ht->array[index]->value);
 		free(ht->array[index]);
-		return (1);
 	}
 	ht->array[index] = new_elem;
 	return (1);
