cab71f1fa5fd24e3d8cd526fc6ae0fce3bfb9601
Judhunja <judxhunja@gmail.com>
2023-07-07 20:10:51 +0300

    retrieves a value associated with a key

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,9 +12,12 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned int index;
 
+	if (ht == NULL || key == NULL)
+		return (NULL);
+
 	index = key_index((unsigned char *)key, ht->size);
 
-	if (ht->array[index] == NULL || index == 0 || index > ht->size)
+	if (ht->array[index] == NULL)
 		return (NULL);
 
 	return (ht->array[index]->value);
