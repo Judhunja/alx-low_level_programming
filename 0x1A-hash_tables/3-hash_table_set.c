eada73a76921d5cc2e4956c6a3440b2fd2502c2a
Judhunja <judxhunja@gmail.com>
2023-07-07 19:37:15 +0300

    adds an element to the hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,23 +15,24 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned int index;
 	char *new_val;
 
-	if (key == NULL || ht == NULL || value == NULL || key[0] == '\0')
+	if (key == NULL || ht == NULL || value == NULL
+			|| key[0] == '\0' || strcmp(key, "") == 0)
 		return (0);
 
 	new_elem = malloc(sizeof(hash_node_t));
 	index = key_index((unsigned char *)key, ht->size);
-
 	if (new_elem == NULL)
 		return (0);
 	new_elem->key = strdup(key);
 	new_elem->value = strdup(value);
 	new_elem->next = NULL;
-
 	if (new_elem->key == NULL || new_elem->value == NULL)
 	{
 		free(new_elem);
 		return (0);
 	}
+	if (ht->array[index] == NULL)
+		ht->array[index] = new_elem;
 	current = ht->array[index];
 	while (current != NULL)
 	{
