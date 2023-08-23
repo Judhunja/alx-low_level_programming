960057326c4ed6bcb5bceb3682bf32251274fa35
Judhunja <judxhunja@gmail.com>
2023-08-23 22:50:50 +0300

    island perimeter of cells

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -5,7 +5,7 @@
 def island_perimeter(grid):
     """ This function returns the perimeter of island described
     in grid. grid is a list of ints:
-       -0 represents a water zone
+        -0 represents a water zone
        -1 represents a land zone
        -One cell is a square with side length 1
        -Grid cells are connected horizontally/vertically (not diagonally)
@@ -14,16 +14,27 @@ def island_perimeter(grid):
     island or nothing.
     The island doesn't have lakes
     """
-    if len(grid) <= 100:
-        if len(grid[0]) <= 100:
-            for i in grid:
-                if i[0] == 0 and i[len(i) - 1] == 0:
-                    for x in range(len(i) - 2):
-                        x += 1
-                    for y in range(len(grid) - 2):
-                        y += 1
+    if len(grid) <= 100 and len(grid[0]) <= 100:
+        row = len(grid)
+        columns = len(grid[0])
 
-    return x * y
+        per = 0
+
+        for x in range(row):
+            for y in range(columns):
+                if grid[x][y] == 1:
+                    per += 4
+
+                    if x > 0 and grid[x - 1][y] == 1:
+                        per -= 1
+                    if x < row - 1 and grid[x + 1][y] == 1:
+                        per -= 1
+                    if y > 0 and grid[x][y - 1] == 1:
+                        per -= 1
+                    if y < columns - 1 and grid[x][y + 1] == 1:
+                        per -= 1
+
+        return per
 
 
 if __name__ == "__main__":
